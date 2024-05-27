a8c407808507475ea418a7bd9e06d5031cf71b63
Gustavo <your_email@example.com>
2024-05-27 15:22:38 -0300

    Ajustes novos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -117,7 +117,7 @@ $(document).ready(function() {
         var picker = new Pikaday({
             field: document.getElementById('data'),
             defaultDate: new Date(),   
-            setDefaultDate: true,     
+            setDefaultDate: false,     
             yearRange: [1950, new Date().getFullYear()],
             format: 'DD/MM/YYYY',
             toString(date, format) {
@@ -189,6 +189,15 @@ $(document).ready(function() {
 
     }
 
+    if (window.location.pathname.includes('listagem-parceiros.html')) {
+        let params = getURLParams();
+        if(params.theme && params.theme!=='undefined'){
+            $('body').addClass(params.theme);
+            $('.logo-datapact').addClass('d-none');
+            $(`.logo-${params.theme}`).addClass('d-block');
+        }
+    }
+
 
     function getURLParams() {
         var url = document.location.href;
@@ -209,6 +218,94 @@ $(document).ready(function() {
     
         return data;
     }
+
+
+
+
+
+
+
+
+
+    //owl
+    /*! Carousel Home */
+        var brandSlider = $('#carousel-v2');
+        brandSlider.owlCarousel({
+            stagePadding: 30,
+            items: 1,
+            autoplay: false,
+            autoplayTimeout: 4000,
+            autoplayHoverPause: true,
+            nav: true,
+            loop: true,
+            dots: false,
+            autoHeight: false,
+            navSpeed: 500,
+            navText: ['<svg class="owl-arrow-v2 left" width="21" height="32" viewBox="0 0 21 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M19.69 10.753H7.505l3.833-3.606c.52-.49.52-1.298 0-1.787a1.351 1.351 0 00-1.836 0L3.39 11.11c-.52.49-.52 1.29 0 1.78l6.112 5.75c.51.48 1.326.48 1.836 0 .52-.49.52-1.289 0-1.778l-3.833-3.607H19.69c.703 0 1.31-.545 1.31-1.25 0-.707-.607-1.252-1.31-1.252zm-13.142.379H19.69 6.548zm0 1.745l4.526 4.259-4.526-4.259zm-3.118-.602a.833.833 0 01.224-.89l6.112-5.751a.965.965 0 000 0l-6.112 5.75a.833.833 0 00-.224.891z" clip-rule="evenodd"></path></svg>', '<svg class="owl-arrow-v2 right" width="21" height="32" viewBox="0 0 21 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.31 10.753h12.185l-3.833-3.606a1.219 1.219 0 010-1.787c.51-.48 1.326-.48 1.836 0l6.112 5.75c.52.49.52 1.29 0 1.78l-6.112 5.75c-.51.48-1.326.48-1.836 0a1.209 1.209 0 010-1.778l3.833-3.607H4.31c-.703 0-1.31-.545-1.31-1.25 0-.707.607-1.252 1.31-1.252zm13.142.379H4.31h13.142zm-4.526-5.498a.965.965 0 011.308 0l6.112 5.75-6.112-5.75a.965.965 0 00-1.308 0z" clip-rule="evenodd"></path></svg>'],
+            responsive: {
+                // breakpoint from 480 up
+                480: {
+                    items: 2,
+                    stagePadding: 20,
+                    loop: true,
+                    dots: false,
+                    autoHeight: false
+                },
+                780: {
+                    items: 3,
+                    stagePadding: 0,
+                    loop: true,
+                    dots: false,
+                    autoHeight: false
+                },
+                980: {
+                    items: 3,
+                    stagePadding: 0,
+                    loop: true,
+                    dots: false,
+                    autoHeight: false
+                }
+            }
+        });
+        brandSlider.trigger('to.owl.carousel', 0);
+
+
+        //Coloca first-active e last-active no carousel
+        $('.owl-carousel').trigger('to.owl.carousel', 0);
+        function brandSliderClasses() {
+            $('.owl-carousel').each(function() {
+                var total = $(this).find('.owl-item.active').length;
+                $(this).find('.owl-item').removeClass('first-active');
+                $(this).find('.owl-item').removeClass('last-active');
+                $(this).find('.owl-item.active').each(function(index) {
+                    if (index === 0) {
+                        $(this).addClass('first-active')
+                    }
+                    if (index === total - 1 && total > 1) {
+                        $(this).addClass('last-active')
+                    }
+                })
+            })
+        }
+        brandSliderClasses();
+        $('.owl-carousel').on('translated.owl.carousel', function(event) {
+            brandSliderClasses()
+        });
+        //Clica em um vai para a primeira posição
+        $(document).on('click', '.owl-item', function() {
+            let carousel = $(this).closest('.owl-carousel');
+            let index = $(this).index();
+            let relativeIndex = carousel.data('owl.carousel').relative(index);
+            carousel.trigger('to.owl.carousel', [relativeIndex, 300]);
+        });
+
+
+
+
+
+
+
+
 
 
 
